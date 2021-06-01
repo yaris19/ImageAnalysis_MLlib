35bcb2567d46daa6d2c6b863f3e2bc2324005b9e
Yaris van Thiel <YJ.vanThiel@student.han.nl>
2021-06-01 09:14:21 +0200

    Added comments

--- src/main/scala/MLlibClassifier.scala
+++ src/main/scala/MLlibClassifier.scala
@@ -14,19 +14,23 @@ object MLlibClassifier {
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
+    // read dataset
     val df = sqlContext.read.format("com.databricks.spark.csv")
       .option("inferSchema", "true")
       .option("header", "true")
       .option("sep", ",")
       .load("src/main/resources/creditcard.csv")
 
+    // get only columns v1 to v28 and amount as features
     val columnName = Seq("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17", "V18", "V19", "V20", "V21", "V22", "V23", "V24", "V25", "V26", "V27", "V28", "Amount")
     val featureCols = df.select(columnName.map(name => col(name)): _*)
 
-    val assembler = new VectorAssembler().
-      setInputCols(featureCols.columns).
-      setOutputCol("features")
+    // set input and output cols
+    val assembler = new VectorAssembler()
+      .setInputCols(featureCols.columns)
+      .setOutputCol("features")
 
+    // hyperparameters determined in https://www.kaggle.com/patelatharva/credit-card-transaction-fraud-detection
     val lgb = new LightGBMClassifier()
       .setLearningRate(0.1)
       .setEarlyStoppingRound(100)
@@ -45,42 +49,52 @@ object MLlibClassifier {
       .setObjective("binary")
       .setBaggingSeed(7)
 
+    // create pipeline with assembler (input/output cols) and classifier
     val pipeline = new Pipeline().setStages(Array(assembler, lgb))
 
+    // split dataset, 80% train and 20% test
     val Array(train, test) = df.randomSplit(Array(0.8, 0.2), seed = 7)
 
+    // train the model with train data
     val model = pipeline.fit(train)
 
+    // predict with test data
     val predictions = model.transform(test)
 
+    // show first 10 predictions
     predictions.select("Class", "prediction", "probability").show(10)
 
+    // calculate test area under ROC
     val binaryEvaluator = new BinaryClassificationEvaluator().setLabelCol("Class").setMetricName("areaUnderROC")
     val testUnderROC = binaryEvaluator.evaluate(predictions)
     println("Test Area Under ROC: %.5f\n".format(testUnderROC))
 
+    // get the tp's, tn's, fp's, fn's
     val tp = predictions.filter(col("Class").equalTo(1) && col("prediction").equalTo(1.0)).count()
     val tn = predictions.filter(col("Class").equalTo(0) && col("prediction").equalTo(0.0)).count()
     val fp = predictions.filter(col("Class").equalTo(0) && col("prediction").equalTo(1.0)).count()
     val fn = predictions.filter(col("Class").equalTo(1) && col("prediction").equalTo(0.0)).count()
-    println("TP: " + tp)
-    println("TN: " + tn)
-    println("FP: " + fp)
-    println("FN: " + fn)
-    print("Total: " + predictions.count() + "\n")
+    println("TP: %s".format(tp))
+    println("TN: %s".format(tn))
+    println("FP: %s".format(fp))
+    println("FN: %s".format(fn))
+    println("Total: %s\n".format(predictions.count()))
 
+    // calculate recall
     var recall: Double = -1.0
     if (tp + fn > 0) {
       recall = tp.toDouble / (tp.toDouble + fn.toDouble)
       println("Recall: %.5f".format(recall))
     }
 
+    // calculate precision
     var precision: Double = -1.0
     if (tp + fp > 0) {
       precision = tp.toDouble / (tp.toDouble + fp.toDouble)
       println("Precision: %.5f".format(precision))
     }
 
+    // calculate precision
     if (recall != -1.0 && precision != -1.0) {
       val f1 = (2 * precision * recall) / (precision + recall)
       println("F1: %.5f".format(f1))
