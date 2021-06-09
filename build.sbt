a31d925f5318caf3b1c5de147e2da2c97c9a6186
Yaris van Thiel <YJ.vanThiel@student.han.nl>
2021-06-09 10:58:31 +0200

    added jar

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "hello"
+name := "GeneExpressionMLlib"
 
 version := "1.0"
 
--- preprocess_data.py
+++ preprocess_data.py
@@ -4,22 +4,31 @@ import pandas as pd
 # datasets: https://www.kaggle.com/crawford/gene-expression
 
 def preprocess_data():
+    # load data
     train_data = pd.read_csv("src/main/resources/data_set_ALL_AML_train.csv")
     test_data = pd.read_csv("src/main/resources/data_set_ALL_AML_independent.csv")
     labels = pd.read_csv("src/main/resources/actual.csv", index_col="patient")
 
+    # drop 'call' columns
     cols = [col for col in test_data.columns if 'call' in col]
     test = test_data.drop(cols, 1)
     cols = [col for col in train_data.columns if 'call' in col]
     train = train_data.drop(cols, 1)
 
+    # concat train and independent datasets
     patients = [str(i) for i in range(1, 73, 1)]
     df_all = pd.concat([train, test], axis=1)[patients]
+
+    # transpose dataframe
     df_all = df_all.T
 
+    # labels to numeric values
     df_all["patient"] = pd.to_numeric(patients)
+
+    # get labels
     labels["cancer"] = pd.get_dummies(labels.cancer, drop_first=True)
 
+    # merge dataframe with labels
     return pd.merge(df_all, labels, on="patient")
 
 
